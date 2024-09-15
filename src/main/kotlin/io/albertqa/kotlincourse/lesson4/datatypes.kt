864406c7ec75b1935434187bf0ba9a562367d8e3
Albert Gilfanov <wayfarer932009@gmail.com>
2024-09-15 15:56:42 +0200

    homework. lesson 4. datatypes

---
+++ src/main/kotlin/io/albertqa/kotlincourse/lesson4/datatypes.kt
@@ -0,0 +1,42 @@
+package io.albertqa.io.albertqa.kotlincourse.lesson4
+
+var intOne: Int = 42
+var longOne: Long = 98765432123456789L
+var floatOne: Float = 23.45f
+var doubleOne: Double = 0.123456789
+var stringOne: String = "Kotlin & Java"
+var booleanOne: Boolean = false
+var charOne: Char = 'c'
+var shortOne: Short = 500
+var longTwo: Long = 4294967296L
+var floatTwo: Float = 18.0f
+var doubleTwo: Double = -0.001
+var stringTwo: String = "OpenAI"
+“true” //incorrect value
+var charTwo: Char = '9'
+var intTwo: Int = 2048
+var longThree: Long = 10000000000L
+var floatThree: Float = 5.75f
+var doubleThree: Double = 1.414
+var stringThree: String = "Artificial Intelligence"
+var booleanTwo: Boolean = false
+var charThree: Char = '@'
+var shortTwo: Short = 1024
+var longFour: Long = 1234567890123L
+var floatFour: Float = 10.01f
+var doubleFour: Double = -273.15
+var stringFour: String = "SpaceX"
+var booleanThree: Boolean = true
+“🤯” //incorrect value
+‘65535’ //incorrect value
+var longFive: Long = 72057594037927935L
+var floatFive: Float = 2.71828f
+var doubleFive: Double = 101.0101
+var stringFive: String = "Quantum Computing"
+var booleanFour: Boolean = false
+var charFour: Char = 'x'
+var shortThree: Short = 314
+var longSix: Long = 123456789123456789L
+var floatSix: Float = 6.626f
+var doubleSix: Double = 0.007
+var stringSix: String = "Android Studio"
